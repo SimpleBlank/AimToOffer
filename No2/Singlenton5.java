1f4386df60f3b3dbf0dd975b6c2bfcad5311c17a
SimpleBlank <jasonchen0723@outlook.com>
2017-09-15 16:14:02 +0800

    Update Singlenton5.java

--- No2/Singlenton5.java
+++ No2/Singlenton5.java
@@ -1,10 +1,7 @@
 package org.test;
 
-//懒汉式单例模式
-//解决多线程下的问题
-//1.为getInstance方法加synchronized关键字保证线程安全
-//2.双重检查锁定
-//3.静态内部类，此方法利用类加载机制，即保证了线程安全，也避免了synchronized带来的性能影响
+//饿汉模式,在类进行初始化时，已经实例化对象了，确保不会出现线程安全问题
+//此方法使用静态内部类，实现需要的时候在加载
 public class NO2 {
 	private static class InstanceHolder
 	{
