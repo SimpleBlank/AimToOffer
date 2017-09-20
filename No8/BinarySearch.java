8f094ec8a59ab180d9bad94898b39ecc88850fb4
SimpleBlank <jasonchen0723@outlook.com>
2017-09-20 16:14:56 +0800

    Binary Search Update

--- No8/BinarySearch.java
+++ No8/BinarySearch.java
@@ -1,12 +1,15 @@
 package org.java.no8.search;
 
+
 public class BinarySearch {
 	public static void main(String[] args)
 	{
 		int[] init = {1,5,7,9,11,20,22,23,50,99};
 		System.out.println(binarySearch(init, 5));
+		System.out.println(binarySearchWithRecursive(init, 0, init.length-1, 1111));
 	}
 
+	//循环方法
 	private static boolean binarySearch(int[] init, int target)
 	{
 		int length = init.length;
@@ -29,5 +32,29 @@ public class BinarySearch {
 		}
 		
 		return false;
+	}
+	
+	//递归方法
+	private static boolean binarySearchWithRecursive(int[] init, int start, int end, int target)
+	{
+		int middle = (start + end)/2;
+		if (start < end)
+		{
+			if (init[middle] == target)
+			{
+				return true;
+			}
+			else if (init[middle] > target) {
+				end = middle-1;
+				return binarySearchWithRecursive(init, start, end, target);
+			}
+			else {
+				start = middle +1;
+				return binarySearchWithRecursive(init, start, end, target);
+			}
+		}
+		else {
+			return false;
+		}
 	}
 }
